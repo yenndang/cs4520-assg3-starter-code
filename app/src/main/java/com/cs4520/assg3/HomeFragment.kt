78b6277849159056c4220398641852d9fdb2913a
yenlnldang@gmail.com <yenlnldang@gmail.com>
2024-02-24 02:15:44 -0500

    Update build.gradle, HomeFragment, and add databinding to manifest -> Working fine with MVP

--- app/src/main/java/com/cs4520/assg3/HomeFragment.kt
+++ app/src/main/java/com/cs4520/assg3/HomeFragment.kt
@@ -1,9 +1,42 @@
 package com.cs4520.assg3
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cs4520.assignment3.R
+import com.cs4520.assignment3.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    // Declare the view variables at the class level but do not initialize them yet
+    // Declaration of the _binding variable with nullable FragmentHomeBinding type.
+    private var _binding: FragmentHomeBinding? = null
 
+    // A non-nullable binding property using Kotlin's backing property technique.
+    // It throws an IllegalStateException if _binding is accessed while null, which should never happen.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Initialize _binding with the inflated layout.
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding?.mvpButton?.setOnClickListener {findNavController().navigate(R.id.action_homeFragment_to_MVPFragment)}
+        _binding?.mvvmButton?.setOnClickListener {findNavController().navigate(R.id.action_homeFragment_to_MVVMFragment)}
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
